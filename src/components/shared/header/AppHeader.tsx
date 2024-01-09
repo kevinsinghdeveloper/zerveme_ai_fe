d9a80a2a1665be8dc84955d82829825521dc7847
Kevin Singh <kevin.singh@answerrocket.com>
2024-01-09 08:22:43 -0500

    make tailwind work!

--- src/components/shared/header/AppHeader.tsx
+++ src/components/shared/header/AppHeader.tsx
@@ -2,16 +2,16 @@ import {PropsWithChildren} from "react";
 import {Container, Nav, Navbar} from "react-bootstrap";
 
 export default function AppHeader(props: PropsWithChildren) {
-
+// TODO Add props to this so we can specify the stlyes below
     return (
-        <header className="border-4 border-red-600">
+        <header style={{width: '90%'}} className="ml-auto mr-auto">
 
-            <Navbar expand="lg" className="bg-body-tertiary">
-                <Container>
+            <Navbar expand="xl" className="bg-body-tertiary">
+                <Container fluid>
                     <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav>
+                        <Nav className="ml-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
 
