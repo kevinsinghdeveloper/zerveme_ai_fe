81bf00b4959b8916f2b0014a6d1dcef6c073379b
kevin singh <kevinsingh.developer@gmail.com>
2024-01-08 23:30:46 -0500

    init 4

--- src/components/shared/header/AppHeader.tsx
+++ src/components/shared/header/AppHeader.tsx
@@ -6,7 +6,7 @@ export default function AppHeader(props: PropsWithChildren) {
     return (
         <header>
 
-            <nav className="navbar navbar-expand-lg navbar-light fixed-top bg-white">
+            <nav className="">
                 <div>
                 </div>
             </nav>
