d75cb9d98861f862414622034c74d8eccd80f890
Kevin Singh <kevin.singh@answerrocket.com>
2024-01-10 17:05:00 -0500

    revert slideshow

--- src/components/pages/landing/LandingPage.tsx
+++ src/components/pages/landing/LandingPage.tsx
@@ -7,7 +7,7 @@ import HomePage from "../HomePage/HomePage";
 export default function LandingPage(props: PropsWithChildren) {
     return (
         <>
-            <AppHeader style={{width: '90%'}} className="ml-auto mr-auto"></AppHeader>
+            <AppHeader style={{width: '90%'}} className="ml-auto mr-auto mt-4"></AppHeader>
             <body style={{width: '90%'}} className="ml-auto mr-auto">
             <Routes>
                 <Route path="/contactus" element={<ContactUsPage/>}/>
