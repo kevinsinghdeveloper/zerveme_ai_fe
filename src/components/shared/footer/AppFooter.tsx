6a1ea3722d07804e5d2b6a1102d4d0b569159bcd
Kevin Singh <kevin.singh@answerrocket.com>
2024-01-11 15:41:26 -0500

    zrv-187 clean up slide images

--- src/components/shared/footer/AppFooter.tsx
+++ src/components/shared/footer/AppFooter.tsx
@@ -1,5 +1,7 @@
 import {PropsWithChildren} from "react";
 import {Col, Container, Row} from "react-bootstrap";
+//import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+//import {faZ} from '@fortawesome/free-solid-svg-icons'
 
 export default function AppFooter(props: PropsWithChildren<{ className?: any, style?: any }>) {
     return (
@@ -10,7 +12,10 @@ export default function AppFooter(props: PropsWithChildren<{ className?: any, st
                         <p>@ 2024 ZerveMe</p>
                     </Col>
                     <Col md={2}>
-                        <p className="text-center font-bold">We Zerve Data</p>
+                        <p className="text-center font-bold">
+                            We Zerve
+                            Data
+                        </p>
                     </Col>
                     <Col md={5}>
                         <p className="float-right">ZerveMeData.com</p>
