8d52df00eab28d1c0d455970b8e37ed40c969931
Kevin Singh <kevin.singh@answerrocket.com>
2024-01-11 14:19:36 -0500

    zrv-187 clean up slide images

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -114,14 +114,14 @@ export default function HomePage(props: PropsWithChildren) {
                     </Row>
                 </Container>
             </Row>
-            <Row className="mt-20">
+            <Row className="!mt-24">
                 <Container fluid>
                     <Row>
                         <Col md={4}>
                             <Card className="w-100">
                                 <CardHeader className="bg-transparent border-0">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor"
-                                         viewBox="0 0 16 16" className="bi bi-graph-up text-9xl">
+                                         viewBox="0 0 16 16" className="bi bi-graph-up text-9xl mr-auto">
                                         <path
                                             d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"></path>
                                     </svg>
