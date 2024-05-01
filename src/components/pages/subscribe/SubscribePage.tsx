c217f7326de8eaaddddb1168b8d4e51e353bfbff
kevin singh <kevinsingh.developer@gmail.com>
2024-05-01 18:47:56 -0400

    ZRV-192 - New subscription page component

---
+++ src/components/pages/subscribe/SubscribePage.tsx
@@ -0,0 +1,25 @@
+import React, {PropsWithChildren, useState} from "react";
+import {
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    CardText,
+    CardTitle,
+    Col,
+    Container,
+    Form,
+    Image,
+    Row
+} from "react-bootstrap";
+import chat_zerveme from "../../../assets/chat_zerveme.png";
+
+
+export default function SubscribePage(props: PropsWithChildren) {
+    
+    return (
+        <Container fluid className="w-96">
+
+        </Container>
+    )
+}
