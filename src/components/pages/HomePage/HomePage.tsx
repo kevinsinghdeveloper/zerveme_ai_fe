016b9fdd2efc462e0a8e401abd5184e5686467b9
kevin singh <kevinsingh.developer@gmail.com>
2024-12-04 19:26:03 -0500

    Edit homepage text2

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -18,33 +18,7 @@ import previewexplorecomp from '../../../assets/previewexplorecomp.png'
 import FrequentQuestionsComponent from "../../shared/components/FrequentQuestionsComponent";
 
 export default function HomePage(props: PropsWithChildren) {
-    const fa_questions = {
-        1: {
-            question: "What is ETL?",
-            answer: "ETL stands for Extract, Transform, Load. It refers to the process of extracting data from various sources, transforming it into a usable format, and loading it into a destination database or data warehouse."
-        },
-        2: {
-            question: "What types of data sources can your ETL solution handle?",
-            answer: "Our ETL solution can handle a wide range of data sources including relational databases, flat files (such as CSV, Excel), cloud storage (like Amazon S3, Google Cloud Storage), APIs, and streaming data."
-        },
-        3: {
-            question: "How does your ETL solution ensure data quality?",
-            answer: "We employ various techniques to ensure data quality including data profiling, cleansing, validation, and enrichment. Additionally, we provide monitoring and error handling mechanisms to identify and rectify any issues during the ETL process."
-        },
-        4: {
-            question: "What scalability options does your ETL solution offer?",
-            answer: "Our ETL solution is designed to scale horizontally and vertically based on the volume and complexity of data processing needs. It can seamlessly handle large volumes of data and accommodate increasing workload demands."
-        },
-        5: {
-            question: "Can your ETL solution integrate with existing data systems?",
-            answer: "Yes, our ETL solution is highly interoperable and can integrate with a wide range of existing data systems including data lakes, data warehouses, business intelligence tools, and enterprise applications. We provide flexible integration options to suit your specific requirements."
-        },
-        6: {
-            question: "What analytics capabilities are available with your ETL environment?",
-            answer: "Our ETL environment offers robust analytics capabilities including data visualization, reporting, ad-hoc querying, and predictive analytics. It empowers businesses to derive actionable insights from their transformed data for informed decision-making."
-        }
-    };
-
+    
 
     return (
         <Container fluid className="w-96">
@@ -105,13 +79,11 @@ export default function HomePage(props: PropsWithChildren) {
                                            src={chartpng}/>
                                 </CardHeader>
                                 <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Data
-                                        Orchestration</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>Converting data into something
-                                        meaningful has always been
-                                        the a complex topic for an organization. Having the best data strategy is
-                                        important to take something that might not seem meaningful and deciphering it to
-                                        extract key insights.</CardText>
+                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Competitor
+                                        analysis</b></CardTitle>
+                                    <CardText style={{backgroundColor: "#121418"}}>
+                                        Analyze how competitors are doing, and how you can surpass them.
+                                    </CardText>
                                 </CardBody>
                             </Card>
                         </Col>
@@ -123,12 +95,10 @@ export default function HomePage(props: PropsWithChildren) {
                                            src={piechart}/>
                                 </CardHeader>
                                 <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Data Architecture and
-                                        Engineering</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>Designing the best data models to
-                                        effectively handle many
-                                        different use cases and building out an ETL or ELT solution to transform and
-                                        load data.&nbsp;<br/></CardText>
+                                    <CardTitle style={{backgroundColor: "#121418"}}><b>AI Ranking score</b></CardTitle>
+                                    <CardText style={{backgroundColor: "#121418"}}>Track your ranking over time, we pull
+                                        data at any frequency you need. So you
+                                        can see your progress daily, weekly, monthly, etc.<br/></CardText>
                                 </CardBody>
                             </Card>
                         </Col>
@@ -140,35 +110,16 @@ export default function HomePage(props: PropsWithChildren) {
                                            src={chartpng}/>
                                 </CardHeader>
                                 <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Web API
-                                        development</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>APIs to retrieve / update data on a
-                                        database</CardText>
+                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Recommendations to
+                                        improve</b></CardTitle>
+                                    <CardText style={{backgroundColor: "#121418"}}>We will provided places where you can
+                                        target to improve your AI score, including other items that will make a top
+                                        player in your area.</CardText>
                                 </CardBody>
                             </Card>
                         </Col>
                     </Row>
                 </Container>
-            </Row>
-            <Row className="!mt-60">
-                <Container fluid>
-                    <Row>
-                        <Col sm={6}>
-                            <Image src={previewexplorecomp} className="ml-auto mr-14"/>
-                        </Col>
-                        <Col sm={6}>
-                            <p className="mr-auto ml-14 text-white">
-                                <p className="text-4xl z-1">Explore ALL kinds of data</p>
-                                <p className="z-1">So we are making it super simple to access any computer you want to
-                                    get your work
-                                    done at the most reasonable prices.</p>
-                            </p>
-                        </Col>
-                    </Row>
-                </Container>
-            </Row>
-            <Row className="!mt-60">
-                <FrequentQuestionsComponent fa_title="Frequenctly Asked Questions" fa_questions={fa_questions}/>
             </Row>
         </Container>
 
