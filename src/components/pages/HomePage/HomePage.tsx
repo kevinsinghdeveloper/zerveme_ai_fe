07b17fa1ed9e7be67ba67a1b1d101280db960377
kevin singh <kevinsingh.developer@gmail.com>
2024-12-04 22:59:25 -0500

    Edit homepage icon

--- src/components/pages/HomePage/HomePage.tsx
+++ src/components/pages/HomePage/HomePage.tsx
@@ -11,14 +11,14 @@ import {
     Row
 } from "react-bootstrap";
 
-import previewdataexplore from '../../../assets/previewdataexplore.png'
+import homepage_dash from '../../../assets/homepage_dash.png'
 import chartpng from '../../../assets/chart.png'
 import piechart from '../../../assets/pie_chart.png'
 import previewexplorecomp from '../../../assets/previewexplorecomp.png'
 import FrequentQuestionsComponent from "../../shared/components/FrequentQuestionsComponent";
 
 export default function HomePage(props: PropsWithChildren) {
-    
+
 
     return (
         <Container fluid className="w-96">
@@ -37,7 +37,8 @@ export default function HomePage(props: PropsWithChildren) {
                                                     Let <span style={{color: '#B660FE'}}> ZerveMeData </span>
                                                     help you stand out.<br/><br/>Optimize your AI search ranking today.
                                                 </h1>
-                                                <Button style={{background: '#B660FE'}}>Join Now!</Button>
+                                                <Button className="mt-10" style={{background: '#B660FE'}}>Join
+                                                    Now!</Button>
                                             </div>
 
                                         </Row>
@@ -54,7 +55,7 @@ export default function HomePage(props: PropsWithChildren) {
                                                  }}>
                                                 <div className="position-absolute top-0 left-0 w-full h-full"
                                                      style={{
-                                                         background: `url(${previewdataexplore}) center / contain no-repeat`,
+                                                         background: `url(${homepage_dash}) center / contain no-repeat`,
                                                          height: '100%',
                                                          maxHeight: 700
                                                      }}>
@@ -68,59 +69,59 @@ export default function HomePage(props: PropsWithChildren) {
                     </Row>
                 </Container>
             </Row>
-            <Row className="!mt-60">
-                <Container fluid>
-                    <Row>
-                        <Col md={4}>
-                            <Card className="w-75 h-100 ml-auto mr-auto text-white"
-                                  style={{backgroundColor: "#121418"}}>
-                                <CardHeader className="bg-transparent border-0">
-                                    <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"
-                                           src={chartpng}/>
-                                </CardHeader>
-                                <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Competitor
-                                        analysis</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>
-                                        Analyze how competitors are doing, and how you can surpass them.
-                                    </CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card className="w-75 h-100 ml-auto mr-auto text-white"
-                                  style={{backgroundColor: "#121418"}}>
-                                <CardHeader className="bg-transparent border-0">
-                                    <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"
-                                           src={piechart}/>
-                                </CardHeader>
-                                <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>AI Ranking score</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>Track your ranking over time, we pull
-                                        data at any frequency you need. So you
-                                        can see your progress daily, weekly, monthly, etc.<br/></CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card className="w-75 h-100 ml-auto mr-auto text-white"
-                                  style={{backgroundColor: "#121418"}}>
-                                <CardHeader className="bg-transparent border-0">
-                                    <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"
-                                           src={chartpng}/>
-                                </CardHeader>
-                                <CardBody style={{backgroundColor: "#121418"}}>
-                                    <CardTitle style={{backgroundColor: "#121418"}}><b>Recommendations to
-                                        improve</b></CardTitle>
-                                    <CardText style={{backgroundColor: "#121418"}}>We will provided places where you can
-                                        target to improve your AI score, including other items that will make a top
-                                        player in your area.</CardText>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
-                </Container>
-            </Row>
+            {/*<Row className="!mt-60">*/}
+            {/*    <Container fluid>*/}
+            {/*        <Row>*/}
+            {/*            <Col md={4}>*/}
+            {/*                <Card className="w-75 h-100 ml-auto mr-auto text-white"*/}
+            {/*                      style={{backgroundColor: "#121418"}}>*/}
+            {/*                    <CardHeader className="bg-transparent border-0">*/}
+            {/*                        <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"*/}
+            {/*                               src={chartpng}/>*/}
+            {/*                    </CardHeader>*/}
+            {/*                    <CardBody style={{backgroundColor: "#121418"}}>*/}
+            {/*                        <CardTitle style={{backgroundColor: "#121418"}}><b>Competitor*/}
+            {/*                            analysis</b></CardTitle>*/}
+            {/*                        <CardText style={{backgroundColor: "#121418"}}>*/}
+            {/*                            Analyze how competitors are doing, and how you can surpass them.*/}
+            {/*                        </CardText>*/}
+            {/*                    </CardBody>*/}
+            {/*                </Card>*/}
+            {/*            </Col>*/}
+            {/*            <Col md={4}>*/}
+            {/*                <Card className="w-75 h-100 ml-auto mr-auto text-white"*/}
+            {/*                      style={{backgroundColor: "#121418"}}>*/}
+            {/*                    <CardHeader className="bg-transparent border-0">*/}
+            {/*                        <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"*/}
+            {/*                               src={piechart}/>*/}
+            {/*                    </CardHeader>*/}
+            {/*                    <CardBody style={{backgroundColor: "#121418"}}>*/}
+            {/*                        <CardTitle style={{backgroundColor: "#121418"}}><b>AI Ranking score</b></CardTitle>*/}
+            {/*                        <CardText style={{backgroundColor: "#121418"}}>Track your ranking over time, we pull*/}
+            {/*                            data at any frequency you need. So you*/}
+            {/*                            can see your progress daily, weekly, monthly, etc.<br/></CardText>*/}
+            {/*                    </CardBody>*/}
+            {/*                </Card>*/}
+            {/*            </Col>*/}
+            {/*            <Col md={4}>*/}
+            {/*                <Card className="w-75 h-100 ml-auto mr-auto text-white"*/}
+            {/*                      style={{backgroundColor: "#121418"}}>*/}
+            {/*                    <CardHeader className="bg-transparent border-0">*/}
+            {/*                        <Image style={{backgroundColor: "#121418"}} className="ml-auto mr-auto"*/}
+            {/*                               src={chartpng}/>*/}
+            {/*                    </CardHeader>*/}
+            {/*                    <CardBody style={{backgroundColor: "#121418"}}>*/}
+            {/*                        <CardTitle style={{backgroundColor: "#121418"}}><b>Recommendations to*/}
+            {/*                            improve</b></CardTitle>*/}
+            {/*                        <CardText style={{backgroundColor: "#121418"}}>We will provided places where you can*/}
+            {/*                            target to improve your AI score, including other items that will make a top*/}
+            {/*                            player in your area.</CardText>*/}
+            {/*                    </CardBody>*/}
+            {/*                </Card>*/}
+            {/*            </Col>*/}
+            {/*        </Row>*/}
+            {/*    </Container>*/}
+            {/*</Row>*/}
         </Container>
 
     )
